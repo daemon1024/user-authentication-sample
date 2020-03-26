6e9e5bfffade53018739baf080e8dcfb7e8c351d
daemon1024 <barun1024@gmail.com>
2020-03-26 18:07:14 +0530

    setting up database

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+mongoose
+  .connect("mongodb://localhost:27017/stickman_backend", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Successfully connect to MongoDB."))
+  .catch(err => console.error("Connection error", err));
+
+const UserSchema = new mongoose.Schema({
+  nickname: String,
+  email: String,
+  password: String,
+  pic: String
+});
+
+const Users = mongoose.model("user", UserSchema);
+
 const app = express();
 
 const port = 4096;
