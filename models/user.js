1b68e7d62f7f7c87adc800cefe3e510922263535
daemon1024 <barun1024@gmail.com>
2020-03-26 23:59:16 +0530

    Improve user model

--- models/user.js
+++ models/user.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  nickname: String,
-  email: String,
-  password: String,
-  pic: String
+  nickname: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  },
+  password: { type: String, required: true }
+  //pic: String
 });
 
 module.exports = mongoose.model("user", UserSchema);
