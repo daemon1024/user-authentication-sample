2469b053afe63365b9cd850680574c8916e75fee
daemon1024 <barun1024@gmail.com>
2020-03-27 12:17:36 +0530

    Added token authorisation

---
+++ routes/auth.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+router.post("/signup", (req, res) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length) {
+        return res.json({
+          message: "E-Mail exists"
+        });
+      } else {
+        bcrypt.hash(req.body.password, 7, (err, hash) => {
+          if (err) {
+            res.status(500).json({ error: err });
+          } else {
+            const user = new User({
+              nickname: req.body.nickname,
+              email: req.body.email,
+              password: hash
+            });
+            user
+              .save()
+              .then(result => res.status(201).json(result))
+              .catch(
+                err => console.log(err) && res.status(500).json({ error: err })
+              );
+          }
+        });
+      }
+    });
+});
+router.post("/login", (req, res) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (!user.length) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        const token = jwt.sign(
+          {
+            email: user[0].email,
+            userId: user[0]._id
+          },
+          "xyz",
+          {
+            expiresIn: "1h"
+          }
+        );
+        if (err) {
+          console.log(err);
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        if (result) {
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed"
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const checkAuth = require("./middleware/checkAuth");
 
 mongoose
   .connect("mongodb://localhost:27017/stickman_backend", {
@@ -11,12 +12,12 @@ mongoose
   .catch(err => console.error("Connection error", err));
 
 const app = express();
-const userRoutes = require("./routes/user");
+const userAuth = require("./routes/auth");
 
 const port = 4096;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/user", userRoutes);
+app.use("/", userAuth);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
